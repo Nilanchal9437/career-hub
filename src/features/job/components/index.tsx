25f59685624a0f5355f37e6971dac68f5023b5a1
Nilanchal9437 <nilanchal.patra3777@gmail.com>
2025-09-01 12:45:06 +0530

    fixes

--- src/features/job/components/index.tsx
+++ src/features/job/components/index.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react";
 import Link from "next/link";
+
 import CustomPagination from "@/components/Pagination";
 import Container from "@/components/Container";
 import { getCookie } from "@/libs/Cookie";
@@ -17,7 +18,7 @@ import {
 import debounce from "lodash/debounce";
 
 function getRepeatIntervalCode(days: number) {
-  const seconds = days * 24 * 60 * 60; // Convertir les jours en secondes
+  const seconds = days * 24 * 60 * 60; // Convert days to seconds
   return `r${seconds}`;
 }
 interface JobPost {
@@ -25,7 +26,7 @@ interface JobPost {
   company: string;
   location: string;
   workplaceType: string | null;
-  postDate: string;
+  postDate: string; // ISO date format e.g. "2025-07-02"
   salary: string | null;
   jobType: string | null;
   jobLink: string;
@@ -46,8 +47,8 @@ interface JobPost {
 }
 
 export default function Jobs() {
-  const [jobTitle, setJobTitle] = React.useState("Développeur logiciel");
-  const [location, setLocation] = React.useState("États-Unis");
+  const [jobTitle, setJobTitle] = React.useState("Software developer");
+  const [location, setLocation] = React.useState("United States");
   const [publication, setPublication] = React.useState(
     getRepeatIntervalCode(30)
   );
@@ -123,37 +124,38 @@ export default function Jobs() {
       )}; expires=${expires.toUTCString()}; path=/`;
     };
     handleSetCookie();
-  }, 1000);
+    getJobList();
+  }, 3000);
 
   const handleCopy = async (textToCopy: string) => {
     try {
       await navigator.clipboard.writeText(textToCopy);
     } catch (err) {
-      console.error("Impossible de copier le texte : ", err);
+      console.error("Failed to copy text: ", err);
     }
   };
 
   return (
     <Container className="mb-10">
-      {/* Section Titre */}
+      {/* Title Section */}
       <section className="pt-8 md:pt-12">
         <div className="flex items-center gap-3 mb-2">
           <span className="inline-flex items-center justify-center rounded-full bg-green-100 w-10 h-10">
             <Laptop className="text-green-500" />
           </span>
           <h1 className="font-extrabold text-2xl md:text-3xl text-gray-900 flex items-center gap-2">
-            Découvrez les dernières offres d’emploi en IT & Logiciel
+            Explore Latest IT & Software Jobs
           </h1>
         </div>
         <p className="text-gray-500 mb-6 md:mb-8 text-sm md:text-base">
-          Offres d’emploi réelles pour développeurs et professionnels tech —
-          sans inscription requise.
+          Real job openings for developers and tech professionals — no login
+          required.
         </p>
-        {/* Recherche & Filtres */}
+        {/* Search & Filters */}
         <div className="bg-white rounded-xl border border-gray-100 shadow-sm p-4 flex flex-col md:flex-row gap-4 md:gap-6 mb-8">
           <div className="flex-1 min-w-[150px]">
             <label className="block text-xs font-semibold text-gray-700 mb-1">
-              Recherche
+              Search
             </label>
             <input
               type="text"
@@ -162,13 +164,13 @@ export default function Jobs() {
                 setJobTitle(event.target.value);
                 debouncedSearch();
               }}
-              placeholder="Titre de poste ou mot-clé"
+              placeholder="Job title or keyword"
               className="text-gray-900 w-full border border-gray-200 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-green-200 bg-[#f8fafc]"
             />
           </div>
           <div className="flex-1 min-w-[150px]">
             <label className="block text-xs font-semibold text-gray-700 mb-1">
-              Localisation
+              Location
             </label>
             <input
               value={location}
@@ -177,13 +179,13 @@ export default function Jobs() {
                 debouncedSearch();
               }}
               type="text"
-              placeholder="Ville ou pays"
+              placeholder="Location"
               className="text-gray-900 w-full border border-gray-200 rounded-md px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-green-200 bg-[#f8fafc]"
             />
           </div>
           <div className="flex-1 min-w-[150px]">
             <label className="block text-xs font-semibold text-gray-700 mb-1">
-              Durée de publication
+              Publication Duration
             </label>
             <select
               value={publication}
@@ -193,16 +195,15 @@ export default function Jobs() {
               }}
               className=" text-gray-900 w-full border border-gray-200 rounded-md px-3 py-2 text-sm bg-[#f8fafc] focus:outline-none focus:ring-2 focus:ring-green-200"
             >
-              <option value={getRepeatIntervalCode(1)}>
-                Dernières 24 heures
-              </option>
-              <option value={getRepeatIntervalCode(7)}>Dernière semaine</option>
-              <option value={getRepeatIntervalCode(30)}>Dernier mois</option>
+              <option value={getRepeatIntervalCode(1)}>Last 24 hours</option>
+              <option value={getRepeatIntervalCode(7)}>Last Week</option>
+              <option value={getRepeatIntervalCode(30)}>Last Month</option>
+              {/* <option value={getRepeatIntervalCode(365)}>Last Year</option> */}
             </select>
           </div>
           <div className="flex-1 min-w-[150px]">
             <label className="block text-xs font-semibold text-gray-700 mb-1">
-              Type d’emploi
+              Job Type
             </label>
             <select
               value={workPlace}
@@ -212,10 +213,10 @@ export default function Jobs() {
               }}
               className="w-full border border-gray-200 rounded-md px-3 py-2 text-sm bg-[#f8fafc] focus:outline-none focus:ring-2 focus:ring-green-200 text-gray-900"
             >
-              <option value="on_site">Présentiel</option>
-              <option value="remote">Télétravail</option>
-              <option value="hybrid">Hybride</option>
-              <option value="all">Tous types</option>
+              <option value="on_site">On Site</option>
+              <option value="remote">Remote</option>
+              <option value="hybrid">Hybrid</option>
+              <option value="all">All Types</option>
             </select>
           </div>
           <div className="flex w-[100px]">
@@ -223,13 +224,13 @@ export default function Jobs() {
               onClick={() => getJobList()}
               className="h-10 w-full mt-5 bg-green-500 hover:bg-green-600 text-white font-semibold px-4 py-2 rounded-md text-sm transition"
             >
-              Rechercher
+              Search
             </button>
           </div>
         </div>
       </section>
 
-      {/* Liste des offres */}
+      {/* Job Listings */}
       <section className="flex flex-col gap-6">
         {load
           ? [...Array(5)].map((_, i) => (
@@ -245,11 +246,11 @@ export default function Jobs() {
                 key={idx}
                 className="bg-white rounded-xl border border-gray-100 shadow-sm p-5 flex flex-col gap-2 relative"
               >
-                {/* Icônes en haut à droite */}
+                {/* Top right icons */}
                 <div className="absolute top-4 right-4">
                   <button
                     className="text-gray-400 hover:text-green-600 p-1"
-                    title="Partager"
+                    title="Share"
                     onClick={() => handleCopy(job.companyLink)}
                   >
                     <Copy />
@@ -276,7 +277,7 @@ export default function Jobs() {
                       <span className="text-gray-400">
                         <Calendar />
                       </span>
-                      Publié le {job.postDate}
+                      Posted on {job.postDate}
                     </span>
                   </div>
                   <div className="flex flex-wrap flex-row gap-3 mt-6">
@@ -297,7 +298,7 @@ export default function Jobs() {
                     className="bg-green-500 hover:bg-green-600 text-white font-semibold px-5 py-2 rounded-md text-sm flex items-center gap-2 transition w-full md:w-auto justify-center"
                     target="_blank"
                   >
-                    Postuler maintenant{" "}
+                    Apply Now{" "}
                     <span className="text-base">
                       <ExternalLink />
                     </span>
